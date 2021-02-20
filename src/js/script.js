04c96a16b9aa04a679c82307e27d8fe1349e92c8
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-20 14:05:06 +0000

    Slide the cards in on scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+const cards = document.querySelectorAll('.cards__card');
+
+const observerOptions = {
+  threshold: 0.5,
+  //rootMargin: '75%',
+};
+
+const slideIn = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    //console.log(entry);
+    if (entry.isIntersecting) {
+      //console.log('Sliding in');
+      entry.target.classList.add('slide');
+      slideIn.unobserve(entry.target);
+    }
+  });
+}, observerOptions);
+
+cards.forEach((card) => {
+  slideIn.observe(card);
+});
