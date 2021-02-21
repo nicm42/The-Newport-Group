1f3c85d2857b8c21706a0a741073f852ca1e73d3
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-21 17:24:12 +0000

    Put the intersection observer in a separate script

---
+++ src/js/observer.js
@@ -0,0 +1,32 @@
+export default function observer() {
+  const info = document.querySelector('.info__text');
+  const list = document.querySelector('.info__list');
+  const cards = document.querySelectorAll('.cards__card');
+  const owner = document.querySelector('.footer__owner');
+  const map = document.querySelector('.footer__contactmap');
+
+  const observerOptions = {
+    threshold: 0.5,
+  };
+
+  const slideIn = new IntersectionObserver(function (entries) {
+    entries.forEach((entry) => {
+      //console.log(entry);
+      if (entry.isIntersecting) {
+        //console.log('Sliding in');
+        entry.target.classList.add('slide');
+        slideIn.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  slideIn.observe(info);
+  slideIn.observe(list);
+
+  cards.forEach((card) => {
+    slideIn.observe(card);
+  });
+
+  slideIn.observe(owner);
+  slideIn.observe(map);
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,3 @@
-const info = document.querySelector('.info__text');
-const list = document.querySelector('.info__list');
-const cards = document.querySelectorAll('.cards__card');
-const owner = document.querySelector('.footer__owner');
-const map = document.querySelector('.footer__contactmap');
+import observer from './observer.js';
 
-const observerOptions = {
-  threshold: 0.5,
-};
-
-const slideIn = new IntersectionObserver(function (entries) {
-  entries.forEach((entry) => {
-    //console.log(entry);
-    if (entry.isIntersecting) {
-      //console.log('Sliding in');
-      entry.target.classList.add('slide');
-      slideIn.unobserve(entry.target);
-    }
-  });
-}, observerOptions);
-
-slideIn.observe(info);
-slideIn.observe(list);
-
-cards.forEach((card) => {
-  slideIn.observe(card);
-});
-
-slideIn.observe(owner);
-slideIn.observe(map);
+observer();
