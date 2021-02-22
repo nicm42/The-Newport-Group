44625316af73437a7acc2d7e4a8b9e27274561bc
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-22 17:57:14 +0000

    Sorted out the nav menu for mobiles

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,10 @@
 export default function menu() {
   const menuToggle = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.menu');
-  console.log(menu);
+  const menuLinks = document.querySelector('.menu-links');
+
   menuToggle.addEventListener('click', function () {
     menu.classList.toggle('cross');
+    menuLinks.classList.toggle('show');
   });
 }
