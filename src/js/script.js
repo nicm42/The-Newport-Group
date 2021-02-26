0a55bf171d1e5da618e58bd58c32a388538babf8
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-26 18:43:52 +0000

    Added React package and test div and script

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,16 @@ import dark from './dark.js';
 observer();
 menu();
 dark();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+let App = document.querySelector('.test');
+
+ReactDOM.render(<HelloMessage />, App);
