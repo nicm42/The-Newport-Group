84d561db9ff4ecbaea5dad8ae77a463f19437aad
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-27 16:20:01 +0000

    Put nav into its own function

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,35 +1,10 @@
 import React from 'react';
+import Nav from './Nav';
 
 export default function Header() {
-  const toggleMenu = () => {
-    console.log('Toggling');
-  };
-
   return (
     <>
-      <nav>
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <span className="menu"></span>
-        </div>
-        <ul className="menu-links">
-          <li className="menu-link">
-            <a href="#">Home</a>
-          </li>
-          <li className="menu-link">
-            <a href="#">Buy or sell a house</a>
-          </li>
-          <li className="menu-link">
-            <a href="#">Build a house</a>
-          </li>
-          <li className="menu-link">
-            <a href="#">Rent an office</a>
-          </li>
-        </ul>
-        <div className="header__lightdark">
-          <i className="las la-sun" title="Light mode"></i>
-          <i className="las la-moon" title="Dark mode"></i>
-        </div>
-      </nav>
+      <Nav />
       <div className="header__title">
         <h1>The Newport Group</h1>
       </div>
---
+++ src/js/components/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Nav() {
+  const toggleMenu = () => {
+    console.log('Toggling');
+  };
+
+  return (
+    <nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span className="menu"></span>
+      </div>
+      <ul className="menu-links">
+        <li className="menu-link">
+          <a href="#">Home</a>
+        </li>
+        <li className="menu-link">
+          <a href="#">Buy or sell a house</a>
+        </li>
+        <li className="menu-link">
+          <a href="#">Build a house</a>
+        </li>
+        <li className="menu-link">
+          <a href="#">Rent an office</a>
+        </li>
+      </ul>
+      <div className="header__lightdark">
+        <i className="las la-sun" title="Light mode"></i>
+        <i className="las la-moon" title="Dark mode"></i>
+      </div>
+    </nav>
+  );
+}
