b2c6c6b9a51fa498b3d6116a0e1b98dbcdaadac1
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-27 18:03:10 +0000

    Deleted dark.js since we no longer need it

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import observer from './observer.js';
 import activeNav from './activeNav.js';
-import dark from './dark.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 observer();
 activeNav();
-//dark();
 
 render(<Header />, document.querySelector('header'));
 render(<Footer />, document.querySelector('footer'));
