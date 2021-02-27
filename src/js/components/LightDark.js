4a3c3237358538f263560363e34dae07b1319105
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-27 18:02:25 +0000

    Got dark mode working in React

--- src/js/components/LightDark.js
+++ src/js/components/LightDark.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LightDark() {
+  const [isDark, setIsDark] = useState(checkMode);
+  let mode = localStorage.getItem('mode');
+
+  function checkMode() {
+    if (localStorage.getItem('mode') === 'dark') {
+      //if the last time they were on the site it was dark mode then make it dark
+      //It's light by default, so don't need to worry about that
+      document.body.classList.add('dark');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const setTheme = () => {
+    if (mode === 'dark') {
+      //console.log('making light');
+      document.body.classList.remove('dark');
+      localStorage.setItem('mode', 'light');
+      setIsDark(false);
+    } else {
+      //console.log('making dark');
+      document.body.classList.add('dark');
+      localStorage.setItem('mode', 'dark');
+      setIsDark(true);
+    }
+    mode = localStorage.getItem('mode');
+    console.log('mode now = ' + mode);
+  };
+
   return (
-    <div className="header__lightdark">
-      <i className="las la-sun" title="Light mode"></i>
-      <i className="las la-moon" title="Dark mode"></i>
+    <div className="header__lightdark" onClick={setTheme}>
+      <i
+        className={isDark ? 'las la-sun' : 'las la-sun none'}
+        title="Light mode"
+      ></i>
+      <i
+        className={isDark ? 'las la-moon none' : 'las la-moon'}
+        title="Dark mode"
+      ></i>
     </div>
   );
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import observer from './observer.js';
-import menu from './menu.js';
 import activeNav from './activeNav.js';
 import dark from './dark.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 observer();
-//menu();
 activeNav();
 //dark();
 
