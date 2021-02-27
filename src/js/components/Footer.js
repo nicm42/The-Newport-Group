ceff96796c5a4b344c366716cc92695626b1d2c5
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-27 16:15:06 +0000

    Changed footer to a function

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -2,51 +2,49 @@ import React from 'react';
 import caleb from '/imgs/caleb.jpg';
 import map from '/imgs/map.png';
 
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="footer__owner">
-          <p>Owner: Caleb Nichol</p>
-          <img
-            src={caleb}
-            alt="Owner: Caleb Nichol"
-            className="footer__photo"
-            loading="lazy"
-          />
+export default function Footer() {
+  return (
+    <>
+      <div className="footer__owner">
+        <p>Owner: Caleb Nichol</p>
+        <img
+          src={caleb}
+          alt="Owner: Caleb Nichol"
+          className="footer__photo"
+          loading="lazy"
+        />
+      </div>
+
+      <div className="footer__contactmap">
+        <div className="footer__contact">
+          <p>
+            The Newport Group
+            <br />
+            Newport Beach
+            <br />
+            The O.C.
+            <br />
+            C.A.
+            <br />
+            92660
+          </p>
+
+          <p>
+            <i className="las la-phone"></i> contact@thenewportgroup.com
+          </p>
+
+          <p>
+            <i className="las la-envelope"></i> 555-123-4567
+          </p>
         </div>
 
-        <div className="footer__contactmap">
-          <div className="footer__contact">
-            <p>
-              The Newport Group
-              <br />
-              Newport Beach
-              <br />
-              The O.C.
-              <br />
-              C.A.
-              <br />
-              92660
-            </p>
-
-            <p>
-              <i className="las la-phone"></i> contact@thenewportgroup.com
-            </p>
-
-            <p>
-              <i className="las la-envelope"></i> 555-123-4567
-            </p>
-          </div>
-
-          <img
-            src={map}
-            alt="Map of Newport Beach"
-            className="footer__map"
-            loading="lazy"
-          />
-        </div>
-      </>
-    );
-  }
+        <img
+          src={map}
+          alt="Map of Newport Beach"
+          className="footer__map"
+          loading="lazy"
+        />
+      </div>
+    </>
+  );
 }
