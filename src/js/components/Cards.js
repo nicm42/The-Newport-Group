d828e2ebdbe2159f5f72d3a8be975e5125edc5b4
Nic <42087659+nicm42@users.noreply.github.com>
2021-02-28 17:25:57 +0000

    Added intersection observer to cards

--- src/js/components/Cards.js
+++ src/js/components/Cards.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import onScreen from '../onScreen';
+
 import mansion from '/imgs/mansion.jpg';
 import construction from '/imgs/construction.jpg';
 import office from '/imgs/office.jpg';
 
 export default function Cards() {
+  const card1Ref = useRef();
+  const card2Ref = useRef();
+  const card3Ref = useRef();
+  const card1IsOnScreen = onScreen(card1Ref);
+  const card2IsOnScreen = onScreen(card2Ref);
+  const card3IsOnScreen = onScreen(card3Ref);
+
   return (
     <section className="cards container">
-      <div className="cards__card">
+      <div
+        ref={card1Ref}
+        className={card1IsOnScreen ? 'cards__card slide' : 'cards__card'}
+      >
         <img src={mansion} alt="" className="cards__image" loading="lazy" />
         <div className="cards__text">
           <button className="cards__button">Buy or sell a house</button>
         </div>
       </div>
-      <div className="cards__card">
+      <div
+        ref={card2Ref}
+        className={card2IsOnScreen ? 'cards__card slide' : 'cards__card'}
+      >
         <img
           src={construction}
           alt=""
@@ -23,7 +38,10 @@ export default function Cards() {
           <button className="cards__button">Build a house</button>
         </div>
       </div>
-      <div className="cards__card">
+      <div
+        ref={card3Ref}
+        className={card3IsOnScreen ? 'cards__card slide' : 'cards__card'}
+      >
         <img src={office} alt="" className="cards__image" loading="lazy" />
         <div className="cards__text">
           <button className="cards__button">Rent offices</button>
--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-//import observer from './observer.js';
-//import activeNav from './activeNav.js';
-
 import Header from './components/Header';
 import Home from './components/Home';
 import Houses from './components/Houses';
 import Buildings from './components/Buildings';
 import Offices from './components/Offices';
 import Footer from './components/Footer';
-
-//observer();
-//activeNav();
-
-//render(<Header />, document.querySelector('header'));
-//render(<Footer />, document.querySelector('footer'));
 
 function App() {
   return (
