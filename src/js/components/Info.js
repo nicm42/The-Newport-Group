f446eacef705fa0b91b8761f09f0c0140f39795c
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-01 18:05:59 +0000

    Added links to the info text

--- src/js/components/Info.js
+++ src/js/components/Info.js
@@ -22,9 +22,7 @@ export default function Info() {
       >
         {list.map((item, index) => (
           <li key={index}>
-            <Link exact to={item.link}>
-              {item.text}
-            </Link>
+            <Link to={item.link}>{item.text}</Link>
           </li>
         ))}
       </ul>
@@ -34,19 +32,20 @@ export default function Info() {
       >
         <p>
           The Newport Group is the foremost Real Estate Agency in the O.C.
-          Looking to <a href="#">buy or sell a luxury home</a> in Newport Beach?
-          We’ve got you covered. Friendly neighborhood, gated community or
-          beachfront property, we have you covered.
+          Looking to <Link to="/houses">buy or sell a luxury home</Link> in
+          Newport Beach? We’ve got you covered. Friendly neighborhood, gated
+          community or beachfront property, we have you covered.
         </p>
         <p>
-          Looking for an <a href="#">office building or office space</a>? We
-          have several suitable buildings on our books, whether you’d prefer to
-          be a business park or by the waterfront.
+          Looking for an{' '}
+          <Link to="/offices">office building or office space</Link>? We have
+          several suitable buildings on our books, whether you’d prefer to be a
+          business park or by the waterfront.
         </p>
         <p>
           No homes suitable for your needs? We are always{' '}
-          <a href="#">building new houses</a>, call us to see what we can do for
-          you.
+          <Link to="/buildings">building new houses</Link>, call us to see what
+          we can do for you.
         </p>
       </div>
     </section>
