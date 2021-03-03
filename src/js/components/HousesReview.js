e92667acfe6b0798ea76fa7e504e7a08f8777ba4
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-03 17:21:25 +0000

    Set up the review part of the houses page

---
+++ src/js/components/HousesReview.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import fireplace from '/imgs/fireplace.jpg';
+import kitchen from '/imgs/kitchen.jpg';
+import lounge from '/imgs/lounge.jpg';
+
+const HousesReview = () => {
+  const reviews = [
+    {
+      image: fireplace,
+      alt: 'An open-plan living room and kitchen with a brick-fireplace',
+      text:
+        '“The Newport Group helped me find the perfect house for our family. I’m in love with the living room, where we can relax in the evenings after the children have gone to bed.”',
+    },
+    {
+      image: kitchen,
+      alt: 'View from the kitchen island, looking into the dining area',
+      text:
+        '“We were looking to downgrade now the children are grown up. The Newport Group found us the perfect house, where we can feel at home.”',
+    },
+    {
+      image: lounge,
+      alt: 'A living area with bijou kictchen',
+      text:
+        '“Our house was on sale with another realtor for three months with no sale, even after reducing the asking price. Then we found The Newport Group and our house was sold within a week, and at $50,000 more than we’d hoped to sell it for.”',
+    },
+  ];
+
+  return (
+    <section className="pageReview container">
+      <p className="pageReview__intro">
+        Don’t take our word for it, see what our customers have been saying...
+      </p>
+      {reviews.map((review, index) => (
+        <div key={index} className="pageReview__imagequote">
+          <img
+            src={review.image}
+            alt={review.alt}
+            className="pageReview__image"
+          />
+          <p className="pageReview__quote">{review.text}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default HousesReview;
--- src/js/pages/Houses.js
+++ src/js/pages/Houses.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import '/css/header-height.css';
+import HousesIntro from '../components/HousesIntro';
+import HousesReview from '../components/HousesReview';
 
-import housekeys from '/imgs/housekeys.jpg';
+import '/css/header-height.css';
 
 const Houses = () => {
   return (
     <>
-      <section className="pageHeader container">
-        <h2 className="pageHeader__title">Buy or sell a house</h2>
-        <div>
-          <img src={housekeys} alt="" className="pageHeader__image" />
-          <div className="pageHeader__intro">
-            <p>We’re the top real estate agents in Newport Beach.</p>
-            <h3>Buying a house?</h3>
-            <p>
-              We have the best properties on our books. From beach properties on
-              Balboa Island, to a commuter-friendly property in Costa Mesa or a
-              nature-side property in Santa Ana Heights. All properties start at
-              4 bedrooms.
-            </p>
-            <p>
-              Houses go quickly, so call us with your requirements and we’ll
-              find something that fits your lifestyle.
-            </p>
-            <h3>Selling a house?</h3>
-            <p>
-              House not for you any more? With premium listings and a minimum of
-              50 photos and one video, we guarantee to sell your house within 30
-              days.
-            </p>
-          </div>
-        </div>
-      </section>
+      <HousesIntro />
+      <HousesReview />
     </>
   );
 };
