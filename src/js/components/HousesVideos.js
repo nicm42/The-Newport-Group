ca1534dc859027f8ae64b8879ed9b9f7017b0626
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-06 17:04:03 +0000

    Made it so pause button only shows on focus & hover

--- src/js/components/HousesVideos.js
+++ src/js/components/HousesVideos.js
@@ -21,19 +21,25 @@ const HousesVideos = () => {
   buttonRefs.current = videos.map(
     (video, i) => buttonRefs.current[i] ?? createRef()
   );
+  const playPauseRefs = useRef([]);
+  playPauseRefs.current = videos.map(
+    (video, i) => playPauseRefs.current[i] ?? createRef()
+  );
 
-  const toggle = (video, button, index) => {
+  const toggle = (video, button, playPause, index) => {
     console.log(isPlaying);
     if (isPlaying[index] === true) {
       setIsPlaying({ ...isPlaying, [index]: false });
       video.current.pause();
-      button.current.classList.remove('la-pause');
-      button.current.classList.add('la-play');
+      playPause.current.classList.remove('la-pause');
+      playPause.current.classList.add('la-play');
+      button.current.style.opacity = 1;
     } else {
       setIsPlaying({ ...isPlaying, [index]: true });
       video.current.play();
-      button.current.classList.remove('la-play');
-      button.current.classList.add('la-pause');
+      playPause.current.classList.remove('la-play');
+      playPause.current.classList.add('la-pause');
+      button.current.style.opacity = 0;
     }
   };
 
@@ -48,18 +54,27 @@ const HousesVideos = () => {
             ref={videoRefs.current[index]}
             className="pageVideos__video"
             onClick={() =>
-              toggle(videoRefs.current[index], buttonRefs.current[index])
+              toggle(
+                videoRefs.current[index],
+                buttonRefs.current[index],
+                playPauseRefs.current[index]
+              )
             }
           >
             <source src={item.video} type="video/mp4" />
           </video>
           <button
             className="pageVideos__control"
+            ref={buttonRefs.current[index]}
             onClick={() =>
-              toggle(videoRefs.current[index], buttonRefs.current[index])
+              toggle(
+                videoRefs.current[index],
+                buttonRefs.current[index],
+                playPauseRefs.current[index]
+              )
             }
           >
-            <i ref={buttonRefs.current[index]} className="las la-play"></i>
+            <i ref={playPauseRefs.current[index]} className="las la-play"></i>
           </button>
         </div>
       ))}
