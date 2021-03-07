959cde3e7a655af7c18cc004fc3a717cf33a3650
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-07 14:46:04 +0000

    Fixed video toggle

--- src/js/utilities/videoToggle.js
+++ src/js/utilities/videoToggle.js
@@ -6,8 +6,8 @@ export const playVideo = (video, button, playPause) => {
 };
 
 export const pauseVideo = (video, button, playPause) => {
-  video.current.play();
-  playPause.current.classList.remove('la-play');
-  playPause.current.classList.add('la-pause');
-  button.current.style.opacity = 0;
+  video.current.pause();
+  playPause.current.classList.remove('la-pause');
+  playPause.current.classList.add('la-play');
+  button.current.style.opacity = 1;
 };
