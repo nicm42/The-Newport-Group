8531b943f9a84859c814c0cdefd35be82407ee04
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-15 10:52:19 +0000

    Make menu disappear when it loses focus

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -7,6 +7,13 @@ const Nav = () => {
   //(won't do anything on desktop as menu-toggle is hidden)
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+  //We want to hide when nothing on the menu has focus
+  //But every time we tab to a new link, the previous one loses focus and will hide the menu
+  //So we tell it to show the menu if a link has focus
+  //Which means hide will only run if no link has focus
+  //(Technically it briefly runs between tabs, but it's so quick we don't see it)
+  const hide = () => setIsOpen(false);
+  const show = () => setIsOpen(true);
 
   //Find height of nav and set the margin-top on body to be that
   const targetRef = useRef();
@@ -24,9 +31,9 @@ const Nav = () => {
   return (
     <nav ref={targetRef}>
       <button className="menu-toggle" onClick={toggle}>
-        <span className={isOpen ? 'menu cross' : 'menu'}></span>
+        <span className={`menu ${isOpen ? 'cross' : ''}`}></span>
       </button>
-      <ul className={isOpen ? 'menu-links show' : 'menu-links'}>
+      <ul className={`menu-links ${isOpen ? 'show' : ''}`}>
         {navigation.map((nav, index) => (
           <li key={index}>
             <NavLink
@@ -34,6 +41,8 @@ const Nav = () => {
               to={nav.link}
               activeClassName="active"
               onClick={toggle}
+              onBlur={hide}
+              onFocus={show}
             >
               {nav.text}
             </NavLink>
