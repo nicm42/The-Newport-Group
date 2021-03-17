45ed552d143af4c53add994b30e25e8c54589c39
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-17 18:02:50 +0000

    Got the test working to test sun and moon icon change on click

--- src/js/components/LightDark.test.js
+++ src/js/components/LightDark.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom';
+
 import LightDark from './LightDark';
+
+//const jsdom = require('jsdom');
+//const { JSDOM } = jsdom;
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Light dark icon switches when button pressed', () => {
   const wrapper = shallow(<LightDark />);
   const button = wrapper.find('button');
-  const sun = wrapper.find('.la-sun');
-  const moon = wrapper.find('.la-moon');
+  let sun = wrapper.find('.la-sun');
+  let moon = wrapper.find('.la-moon');
   it('should all exist', () => {
     expect(sun.length).toBe(1);
     expect(wrapper.find('.none').length).toBe(1);
@@ -19,9 +23,19 @@ describe('Light dark icon switches when button pressed', () => {
     expect(sun.hasClass('none')).toEqual(true);
     expect(moon.hasClass('none')).toEqual(false);
   });
-  it('should show sun icon when clicked', () => {
+  it.only('should show sun icon when clicked', () => {
     button.simulate('click');
+    sun = wrapper.find('.la-sun');
+    moon = wrapper.find('.la-moon');
     expect(sun.hasClass('none')).toEqual(false);
     expect(moon.hasClass('none')).toEqual(true);
+  });
+  it('should update body class when clicked', () => {
+    //const body = document.body;
+    //const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+    //console.log(dom.window.document.body);
+    //expect(dom.window.document.body.hasClass('dark')).toEqual(false);
+    expect(true).toBeTruthy;
+    //expect(document.body.length).toBe(1);
   });
 });
