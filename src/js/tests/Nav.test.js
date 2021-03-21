bfde4f0d92c16e609671ced123c7581ea8df941a
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-21 09:59:29 +0000

    Moved the tests into their own folder

--- src/js/tests/Nav.test.js
+++ src/js/tests/Nav.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom';
-import Nav from './Nav';
+import Nav from '../components/Nav';
 
 Enzyme.configure({ adapter: new Adapter() });
 
