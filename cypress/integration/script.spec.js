5df6b2e99adcf86a99d7512a332ed3f584a7d0df
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-30 20:43:30 +0100

    Tested nav links work

--- cypress/integration/script.spec.js
+++ cypress/integration/script.spec.js
@@ -8,4 +8,23 @@ context('Navigation', () => {
     cy.get('.skip').click({ force: true });
     cy.get('main').should('be.inViewport');
   });
+
+  it.only('Nav links work', () => {
+    //First link is to home
+    //So we go to houses
+    cy.get('.menu-links > li > a').eq(1).click();
+    cy.url().should('eq', 'http://localhost:1234/houses');
+
+    //Now we can go to home
+    cy.get('.menu-links > li > a').eq(0).click();
+    cy.url().should('eq', 'http://localhost:1234/');
+
+    //And buildings
+    cy.get('.menu-links > li > a').eq(2).click();
+    cy.url().should('eq', 'http://localhost:1234/buildings');
+
+    //And offices
+    cy.get('.menu-links > li > a').eq(3).click();
+    cy.url().should('eq', 'http://localhost:1234/offices');
+  });
 });
