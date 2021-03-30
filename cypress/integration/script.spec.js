f9941a6859a9cb9f2282b81f57676d02156b3fa8
Nic <42087659+nicm42@users.noreply.github.com>
2021-03-30 20:48:52 +0100

    Tested info links work

--- cypress/integration/script.spec.js
+++ cypress/integration/script.spec.js
@@ -9,7 +9,7 @@ context('Navigation', () => {
     cy.get('main').should('be.inViewport');
   });
 
-  it.only('Nav links work', () => {
+  it('Nav links work', () => {
     //First link is to home
     //So we go to houses
     cy.get('.menu-links > li > a').eq(1).click();
@@ -26,5 +26,38 @@ context('Navigation', () => {
     //And offices
     cy.get('.menu-links > li > a').eq(3).click();
     cy.url().should('eq', 'http://localhost:1234/offices');
+  });
+
+  it.only('Info links work', () => {
+    //list links
+    //For some reason it thinks the menu is covering everything
+    cy.get('.info__list > li > a').eq(0).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/houses');
+
+    //Need to go back to home page to check next link
+    cy.visit('http://localhost:1234/');
+    cy.get('.info__list > li > a').eq(1).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/buildings');
+
+    //Need to go back to home page to check next link
+    cy.visit('http://localhost:1234/');
+    cy.get('.info__list > li > a').eq(2).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/offices');
+
+    //paragraph links
+    //Need to go back to home page to check next link
+    cy.visit('http://localhost:1234/');
+    cy.get('.info__text > p > a').eq(0).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/houses');
+
+    //Need to go back to home page to check next link
+    cy.visit('http://localhost:1234/');
+    cy.get('.info__text > p > a').eq(1).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/offices');
+
+    //Need to go back to home page to check next link
+    cy.visit('http://localhost:1234/');
+    cy.get('.info__text > p > a').eq(2).click({ force: true });
+    cy.url().should('eq', 'http://localhost:1234/buildings');
   });
 });
